23aa58f4b81c745a8e16465e7e5d548884668df6
Alicia <aliciaxd@live.se>
2023-11-14 14:01:11 +0100

    final changes

--- script.js
+++ script.js
@@ -30,23 +30,28 @@ function checkGuesses(){
     attempts--;
 
     //Kollar om du angett rätt nummer
-    if(guess === randomNumber){
+    if(guess === randomNumber && guess < 101 && guess > 0){
         message.textContent = `Grattis! Du har gissat rätt, talet är ${randomNumber}!`;
         disableInput();
         return;
-    } else if(attempts === 0){
-        message.textContent = `Nu har du slut försök.`;
-        disableInput();
-        return;
-    } else if(guess < randomNumber){
-        //Säger om du gissat för lågt eller högt.
-        const listItem = document.createElement('li');
-        listItem.textContent = `Din gissning är: ${guess}, det är för lågt.`
-        list.appendChild(listItem);
-    } else if(guess > randomNumber){
-        const listItem = document.createElement("li");
-        listItem.textContent = `Din gissning är: ${guess}, det är för högt.`;
-        list.appendChild(listItem);
+    } else if (attempts === 0 && guess < 101 && guess > 0) {
+      message.textContent =
+        guess < randomNumber
+          ? `Din sista gissning var ${guess} och för lågt, det rätta svaret var ${randomNumber}. Nu har du slut försök.`
+          : `Din sista gissning var ${guess} och för högt, det rätta svaret var ${randomNumber}. Nu har du slut försök.`;
+      disableInput();
+      return;
+    } else if (guess < randomNumber && guess < 101 && guess > 0) {
+      //Säger om du gissat för lågt eller högt.
+      const listItem = document.createElement("li");
+      listItem.textContent = `Din gissning är: ${guess}, det är för lågt.`;
+      list.appendChild(listItem);
+    } else if (guess > randomNumber && guess < 101 && guess > 0) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `Din gissning är: ${guess}, det är för högt.`;
+      list.appendChild(listItem);
+    } else {
+      alert("Skriv en siffra mellan 0-100");
     }
 
     guessInput.value = "";
